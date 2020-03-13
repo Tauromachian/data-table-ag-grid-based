2dc1113c727605ea966f280da4bd6b08d073be85
jogarcia <jogarcia@uclv.cu>
2020-03-13 14:05:37 -0400

    Create basic data-table

---
+++ src/components/DataTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container fluid>
+    <slot :search="search" :dense="dense" :rowAmount="rowAmount"></slot>
+
+    <ag-grid-vue
+      style="width: inherit; height: 500px;"
+      class="ag-theme-material"
+      :defaultColDef="defaultColDef"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+      :quickFilterText="search"
+      :pagination="pagination"
+      :paginationPageSize="rowAmount"
+      :gridOptions="gridOptions"
+    ></ag-grid-vue>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "DataTable",
+  props: {
+    defaultColDef: Object,
+    columnDefs: {
+      type: Array,
+      required: true
+    },
+    rowData: {
+      type: Array,
+      required: true
+    },
+  },
+  data() {
+    return {
+      dense: false,
+      gridOptions: {},
+      gridApi: {},
+      pagination: true,
+      rowAmount: 10,
+      search: "",
+    };
+  },
+
+  methods: {
+    setNoDense() {
+      this.dense = false;
+    },
+    setDense() {
+      this.dense = true;
+    }
+  }
+};
+</script>
