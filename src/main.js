33ef091e02728883b9b3440a9119a64a1056a16f
jogarcia <jogarcia@uclv.cu>
2020-03-13 14:05:07 -0400

    Import ag-grid globally

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import { AgGridVue } from "ag-grid-vue";
 
+
+Vue.component('ag-grid-vue', AgGridVue)
 Vue.config.productionTip = false
 
 new Vue({
