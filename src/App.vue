aa964a6ba94d6f55202d6067d276647a0cdc2d4c
jogarcia <jogarcia@uclv.cu>
2020-03-13 14:05:48 -0400

    Import data-table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <data-table :columnDefs="columnDefs" :rowData="rowData">
+    <template v-slot:default="{ search, dense, rowAmount }">
+      <toolbar :search="search" :dense="dense" :rowAmount="rowAmount" />
+    </template>
+  </data-table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataTable from "./components/DataTable";
+import Toolbar from "./components/Toolbar";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
+    DataTable,
+    Toolbar
+  },
+  data: () => ({
+    columnDefs: [],
+    rowData: [],
+    columnFields: [],
+    defaultColDef: null,
+    dialog: false,
+    fluid: true,
+    gridOptions: {},
+    gridApi: null,
+    pagination: true,
+    pageAmounts: [10, 50, 100],
+    rowHeight: 48,
+    search: "",
+    visibleColumns: []
+  }),
+
+  beforeMount() {
+    (this.columnDefs = [
+      {
+        headerName: "Servicio",
+        field: "service",
+        checkboxSelection: true
+      },
+      {
+        headerName: "CRF",
+        field: "clientCode"
+      },
+      {
+        headerName: "Codigo",
+        field: "price"
+      },
+      {
+        headerName: "Codigo1",
+        field: "price1"
+      },
+      {
+        headerName: "Codigo2",
+        field: "price2"
+      }
+    ]),
+      (this.defaultColDef = {
+        sortable: true,
+        resizable: true,
+        filter: true,
+        editable: true
+      });
+
+    this.rowData = [
+      { service: "Planta Piloto", clientCode: "330", price: 35000 },
+      { service: "900", clientCode: "340", price: 32000 },
+      { service: "C1", clientCode: "342", price: 71000 },
+      { service: "C2", clientCode: "343", price: 52000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 },
+      { service: "C3", clientCode: "344", price: 12000 }
+    ];
   }
-}
+};
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-theme-material.css";
 </style>
